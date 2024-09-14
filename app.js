4c23a88d40e8dd7879ef6988c1927069a414e549
kaizz04 <kaveesh@marketpushapps.com>
2024-09-14 09:41:35 +0530

    genarate random num using min.max value

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 //Floating points
 
-console.log(0.2.toFixed(20));
-console.log(0.2.toFixed(2));
-console.log((0.2+0.4).toFixed(2));
+// console.log(0.2.toFixed(20));
+// console.log(0.2.toFixed(2));
+// console.log((0.2+0.4).toFixed(2));
+
+function randomIntBetween(max, min){
+    return Math.random() * (max - min) + min;
+}
+
+console.log(randomIntBetween(10,5));
+
